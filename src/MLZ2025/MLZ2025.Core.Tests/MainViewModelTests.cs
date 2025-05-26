952a3ac52232b41807e4dac7b29a64a538fb1a8b
maru <marcus.knop@outlook.com>
2025-05-26 17:48:50 +0200

    Lernzielkontrolle heruntergeladen

--- src/MLZ2025/MLZ2025.Core.Tests/MainViewModelTests.cs
+++ src/MLZ2025/MLZ2025.Core.Tests/MainViewModelTests.cs
@@ -1,18 +1,12 @@
-﻿using Microsoft.Maui.Networking;
-using MLZ2025.Core.Services;
-using MLZ2025.Core.ViewModel;
+﻿using MLZ2025.Core.ViewModel;
 
 namespace MLZ2025.Core.Tests;
 
 public class MainViewModelTests : TestsBase
 {
-    [TestCase("")]
-    [TestCase(" ")]
-    [TestCase("\t")]
-    [TestCase("\r")]
-    [TestCase("   ")]
-    [TestCase(null)]
-    [TestCase("\n")]
+    private static readonly string?[] EmptyTexts = ["", " ", "\t", "\r", "   ", null, "\n"];
+
+    [TestCaseSource(nameof(EmptyTexts))]
     public void TestCannotAddEmptyText(string? text)
     {
         var serviceProvider = CreateServiceProvider();
@@ -24,40 +18,67 @@ public class MainViewModelTests : TestsBase
         Assert.That(_testDialogService.LastMessage, Is.EqualTo("Please enter a text"));
     }
 
-    [TestCase("")]
-    [TestCase(" ")]
-    [TestCase("\t")]
-    [TestCase("\r")]
-    [TestCase("   ")]
-    [TestCase(null)]
-    [TestCase("\n")]
+    [Test]
+    public void TestCannotAddWithoutInternet()
+    {
+        var serviceProvider = CreateServiceProvider();
+        var viewModel = serviceProvider.GetRequiredService<MainViewModel>();
+        _testConnectivity.NetworkAccess = NetworkAccess.None;
+        viewModel.Text = "Foo";
+
+        viewModel.AddCommand.Execute(null);
+
+        Assert.That(_testDialogService.LastMessage, Is.EqualTo("No Internet. Please check your internet connection."));
+    }
+
+    [TestCaseSource(nameof(EmptyTexts))]
     public void TestCannotSelectEmptyText(string? text)
     {
         var serviceProvider = CreateServiceProvider();
         var viewModel = serviceProvider.GetRequiredService<MainViewModel>();
-        viewModel.Text = text;
 
-        viewModel.SelectCommand.Execute(null);
+        viewModel.SelectCommand.Execute(text);
 
         Assert.That(_testDialogService.LastMessage, Is.EqualTo("Please enter a text"));
     }
 
-    [TestCase("")]
-    [TestCase(" ")]
-    [TestCase("\t")]
-    [TestCase("\r")]
-    [TestCase("   ")]
-    [TestCase(null)]
-    [TestCase("\n")]
+    [TestCaseSource(nameof(EmptyTexts))]
     public void TestCannotDeleteEmptyText(string? text)
     {
         var serviceProvider = CreateServiceProvider();
         var viewModel = serviceProvider.GetRequiredService<MainViewModel>();
-        viewModel.Text = text;
 
-        viewModel.DeleteCommand.Execute(null);
+        viewModel.DeleteCommand.Execute(text);
 
         Assert.That(_testDialogService.LastMessage, Is.EqualTo("Please enter a text"));
+    }
+
+    [Test]
+    public void TestCannotDeleteWithoutInternet()
+    {
+        var serviceProvider = CreateServiceProvider();
+        var viewModel = serviceProvider.GetRequiredService<MainViewModel>();
+        _testConnectivity.NetworkAccess = NetworkAccess.None;
+
+        var item = viewModel.Items.Last();
+
+        viewModel.DeleteCommand.Execute(item);
+
+        Assert.That(_testDialogService.LastMessage, Is.EqualTo("No Internet. Please check your internet connection."));
+    }
+
+    [Test]
+    public void TestCannotSelectWithoutInternet()
+    {
+        var serviceProvider = CreateServiceProvider();
+        var viewModel = serviceProvider.GetRequiredService<MainViewModel>();
+        _testConnectivity.NetworkAccess = NetworkAccess.None;
+
+        var item = viewModel.Items.Last();
+
+        viewModel.SelectCommand.Execute(item);
+
+        Assert.That(_testDialogService.LastMessage, Is.EqualTo("No Internet. Please check your internet connection."));
     }
 
     [Test]
@@ -85,5 +106,21 @@ public class MainViewModelTests : TestsBase
 
         Assert.That(_testDialogService.LastMessage, Is.EqualTo(""));
         Assert.That(viewModel.Items, Does.Not.Contain(item));
+    }
+
+    [Test]
+    public void TestSelectItem()
+    {
+        Application.Current = new App();
+
+        var serviceProvider = CreateServiceProvider();
+        var viewModel = serviceProvider.GetRequiredService<MainViewModel>();
+
+        var item = viewModel.Items.Last();
+
+        viewModel.SelectCommand.Execute(item);
+
+        Assert.That(_testDialogService.LastMessage, Is.EqualTo(""));
+        Assert.That(viewModel.Text, Is.EqualTo(item));
     }
 }
